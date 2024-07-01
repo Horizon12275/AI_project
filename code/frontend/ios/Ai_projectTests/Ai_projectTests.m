dc48ee6acea980fe0545e5a7bd5033d0c47f9ecf
Tianran Sun <Seriousss@sjtu.edu.cn>
2024-07-01 21:46:24 +0800

    login 和 signin 前端

--- code/frontend/ios/Ai_projectTests/Ai_projectTests.m
+++ code/frontend/ios/Ai_projectTests/Ai_projectTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface frontendTests : XCTestCase
+@interface Ai_projectTests : XCTestCase
 
 @end
 
-@implementation frontendTests
+@implementation Ai_projectTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
