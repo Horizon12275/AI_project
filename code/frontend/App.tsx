106ccfad130ba51776a6af888af4a3aabc5f8d3f
Horizon <111064289+Horizon12275@users.noreply.github.com>
2024-07-03 12:00:02 +0800

    更新代码名称

--- code/frontend/App.tsx
+++ code/frontend/App.tsx
@@ -1,8 +1,8 @@
 import {View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import LoginScreen from './src/pages/loginpage';
-import SignUpScreen from './src/pages/signinpage';
+import LoginScreen from './src/screens/log_in_screen';
+import SignUpScreen from './src/screens/sign_up_screen';
 
 
 const Stack = createNativeStackNavigator();
