91b7f808a431937dd986921e3d52626ada51e526
nwdnys1 <zh13588083809@sjtu.edu.cn>
2024-07-19 13:01:37 +0800

    update

--- code/backend/user-service/src/main/java/org/example/client/AIClient.java
+++ code/backend/user-service/src/main/java/org/example/client/AIClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(url = "http://localhost:8000", name = "ai-service")
+@FeignClient(url = "http://124.71.171.197:8000", name = "ai-service")
 public interface AIClient {
     @PostMapping("/generate_reminders")
     String[] generateReminders(@RequestBody EventDetails eventDetails);
