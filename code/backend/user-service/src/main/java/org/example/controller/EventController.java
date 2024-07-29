5a7d899117b8279f15a37a159741520f77dc7785
nwdnys1 <zh13588083809@sjtu.edu.cn>
2024-07-29 11:25:24 +0800

    连接池配置

--- code/backend/user-service/src/main/java/org/example/controller/EventController.java
+++ code/backend/user-service/src/main/java/org/example/controller/EventController.java
@@ -39,10 +39,6 @@ public class EventController {
     public Result<Event> updateEvent(@PathVariable int id, @RequestBody Event event) {
         return service.updateEvent(id, event);
     }
-    @PostMapping("/add_offline")
-    public Result<Event> addEventOffline(@RequestBody Event event) {
-        return Result.success(null);
-    }
     @GetMapping("/summary")
     public Result<List<Object>> summary(@RequestParam LocalDate start,@RequestParam LocalDate end) {
         return service.summary(start,end);
