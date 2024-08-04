c4b02da10f99886684e465d0b3a82dec1f868cb8
ZH <147311198+nwdnys1@users.noreply.github.com>
2024-08-04 16:12:01 +0800

    spring session+redis实现session共享

--- code/backend/common/src/main/java/org/example/entity/User.java
+++ code/backend/common/src/main/java/org/example/entity/User.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Data
@@ -14,7 +15,7 @@ import java.util.List;
 @Entity
 @Table(name = "users")
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler","events","password"})
-public class User {
+public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
